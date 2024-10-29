3c2ae0f06b9b67cf42b2de9934b0338eb5fa8f08
Oskar Høxbroe <oskar.hoxbroe@gmail.com>
2024-10-29 18:51:04 +0100

    adding more game logic pseudocode

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,11 +1,9 @@
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-//i gameUI, instansiera denna klass med "jb.addMouseListener(new MouseAdapterDemo()); för varje knapp
+//i gameUI, instansiera denna klass med "jb.addMouseListener(new GameLogic()); för varje knapp
 
 public class GameLogic extends MouseAdapter {
-    private int currentButton = 0;
-    private boolean buttonClickable = false;
 
     //behövs konstruktor?
     GameLogic() {
@@ -16,29 +14,29 @@ public class GameLogic extends MouseAdapter {
     public void mouseEntered(MouseEvent entered) {
         System.out.println("Mouse entered.");
         //ogiltiga rutor blir ljusröda och de två giltiga blir ljusgröna när man hovrar
-        //anropar checkValidButton, skickar in currentButton och tar emot true eller false
-        //sparar true eller false i klassens variabel
-        //om true, sätt currentButton som ljusgrön samt buttonClickable som true
-        //om false, sätt currentButton som ljusröd samt buttonClickable som false
+
     }
 
     @Override
     public void mouseExited(MouseEvent exited) {
         System.out.println("Mouse exited.");
         //återställer alltid aktuell knapps färg till ursprungsfärg när denna blir aktiverad
-        //sätt alltid currentButton till ursprungsfärg
     }
 
     @Override
     public void mouseClicked(MouseEvent clicked) {
         System.out.println("The mouse was clicked.");
-        //om buttonClickable satts till true av mouseEntered, och användaren trycker, så ska knappen som tryckts
-        //byta plats med den tomma knappen.
+        //1. knappen som tryckts ska byta plats med den tomma knappen.
 
-        //om istället getSource är knappen "Nytt spel" så ska alla brickona blandas i slumpmässig ordning, inga identiska
+        //2. om istället getSource är knappen "Nytt spel" så ska alla brickona blandas i slumpmässig ordning, inga identiska
     }
 
-    //hjälpmetod checkValidButton som avgör om ruta är giltig = ljusgrön eller ogiltig = ljusröd
+    //ON: FOKUSERA på hjälpmetoden checkNearbyButtons först. <- <- <-
+    //hjälpmetod checkNearbyButtons som identifierar knapparna ovanför, under, till höger och till vänster om aktuell knapp i gridLayout
+    //genom att vi har alla knappar i ett rutnät har alla knappar ett fast index som kan räknas ut
+    //den metoden tar emot int index från mouseClicked som skickar aktuell knapps index
+
+    //hjälpmetod checkValidButton som avgör om ruta är giltig eller inte
     boolean checkValidButton(int button) {
         boolean isValid = false;
 
@@ -50,10 +48,7 @@ public class GameLogic extends MouseAdapter {
         return false; //temporärt
     }
 
-    //hjälpmetod checkNearbyButtons som identifierar knapparna ovanför, under, till höger och till vänster om aktuell knapp i gridLayout
-
-    //metod som kontrollerar om alla brickor ligger rätt i nummerordning
-    //Vi testar varje rad för sig, att rad 1 är 1-4, rad 2 är 5-8, rad 3 är 9-12, rad 4 är 13-15 + tom
-
-
+    //till sist metod som kontrollerar om alla brickor ligger rätt i nummerordning
+    //vi testar varje rad för sig, att rad 1 är 1-4, rad 2 är 5-8, rad 3 är 9-12, rad 4 är 13-15 + tom
+    //skriv då ut "Grattis, du vann!”
 }
